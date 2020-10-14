fca66401e3e2fc613a81a254c0e806028c19bb78
Jianxu Chen <jianxuc@n70.corp.alleninst.org>
2020-10-14 10:03:35 -0700

    Initial commit

---
+++ segmenter_model_zoo/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for segmenter_model_zoo."""
+
+__author__ = "Jianxu Chen"
+__email__ = "jianxuc@alleninstitute.org"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.1"
+
+
+def get_module_version():
+    return __version__
+
+
+from .example import Example  # noqa: F401
